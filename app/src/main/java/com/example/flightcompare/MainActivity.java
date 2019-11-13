5c91e83ee16fd60184fa1fcd81f2e6f6aed62e22
amandaf360 <amandaf360@gmail.com>
2019-11-13 10:21:00 -0700

    Original setup of app

--- app/src/main/java/com/example/flightcompare/MainActivity.java
+++ app/src/main/java/com/example/flightcompare/MainActivity.java
@@ -11,6 +11,24 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // start by loading the "search for flights" fragment
+//        if (savedInstanceState == null) {
+//            LoginFragment loginFragment = new LoginFragment();
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.frame, loginFragment)
+//                    .commit();
+//        }
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -18,27 +36,20 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    // need to switch fragments
+                    mTextMessage.setText(R.string.flights_tab);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    // need to switch fragments
+                    mTextMessage.setText(R.string.favorites_tab);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    // need to switch fragments
+                    mTextMessage.setText(R.string.compare_tab);
                     return true;
             }
             return false;
         }
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
 }
