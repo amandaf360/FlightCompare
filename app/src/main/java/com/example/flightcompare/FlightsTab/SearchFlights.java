333a73984de17d353c9eff39bb0b433f3185f68f
amandaf360 <amandaf360@gmail.com>
2019-11-17 22:24:14 -0700

    connected search flights and created fragment file for saved flights

--- app/src/main/java/com/example/flightcompare/FlightsTab/SearchFlights.java
+++ app/src/main/java/com/example/flightcompare/FlightsTab/SearchFlights.java
@@ -3,24 +3,32 @@ package com.example.flightcompare.FlightsTab;
 import android.content.Context;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.flightcompare.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link SearchFlights.OnSearchFlightsSelectedListener
- *} interface
- * to handle interaction events.
- * Use the {@link SearchFlights#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SearchFlights extends Fragment {
 
-    private OnSearchFlightsSelectedListener mListener;
+    // UI elements
+    TextInputEditText fromAirportEdit;
+    TextInputEditText toAirportEdit;
+    TextInputEditText departDateEdit;
+    TextInputEditText returnDateEdit;
+
+    RadioButton roundtripRadio;
+    RadioButton onewayRadio;
+
+    MaterialButton searchButton;
+
 
     public SearchFlights() {}
 
@@ -37,26 +45,140 @@ public class SearchFlights extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_flights, container, false);
+        View v = inflater.inflate(R.layout.fragment_search_flights, container, false);
+
+        fromAirportEdit = v.findViewById(R.id.from_airport_edit);
+        toAirportEdit = v.findViewById(R.id.to_airport_edit);
+        departDateEdit = v.findViewById(R.id.depart_date_edit);
+        returnDateEdit = v.findViewById(R.id.return_date_edit);
+        roundtripRadio = v.findViewById(R.id.roundtrip_radio);
+        onewayRadio = v.findViewById(R.id.oneway_radio);
+        searchButton = v.findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSearchClicked();
+            }
+        });
+
+        RadioGroup tripButtonsGroup = v.findViewById(R.id.radio_group);
+
+
+        searchButton.setEnabled(false);
+
+        TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                searchButton.setEnabled(false);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // if oneway, disable the "return date" field
+                if(onewayRadio.isChecked()) {
+                    returnDateEdit.setEnabled(false);
+                }
+                else {
+                    returnDateEdit.setEnabled(true);
+                }
+
+                // check to see if all fields are filled out
+                if(fromAirportEdit.getText().toString().length() != 0 && toAirportEdit.getText().toString().length() != 0
+                    && departDateEdit.getText().toString().length() != 0) {
+                    if(roundtripRadio.isChecked() && returnDateEdit.getText().toString().length() != 0) {
+                        searchButton.setEnabled(true);
+                    }
+                    else if(onewayRadio.isChecked()) {
+                        searchButton.setEnabled(true);
+                    }
+                }
+                else {
+                    searchButton.setEnabled(false);
+                }
+            }
+        };
+
+        fromAirportEdit.addTextChangedListener(watcher);
+        toAirportEdit.addTextChangedListener(watcher);
+        departDateEdit.addTextChangedListener(watcher);
+        returnDateEdit.addTextChangedListener(watcher);
+//        roundtripRadio.addTextChangedListener(watcher);
+//        onewayRadio.addTextChangedListener(watcher);
+//        searchButton.addTextChangedListener(watcher);
+
+        //Listener -- setOnCheckedChangeListener???
+        RadioGroup.OnCheckedChangeListener listener = new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                // if oneway, disable the "return date" field
+                if(onewayRadio.isChecked()) {
+                    returnDateEdit.setEnabled(false);
+                }
+                else {
+                    returnDateEdit.setEnabled(true);
+                }
+
+                // check to see if all fields are filled out
+                if(fromAirportEdit.getText().toString().length() != 0 && toAirportEdit.getText().toString().length() != 0
+                        && departDateEdit.getText().toString().length() != 0) {
+                    if(roundtripRadio.isChecked() && returnDateEdit.getText().toString().length() != 0) {
+                        searchButton.setEnabled(true);
+                    }
+                    else if(onewayRadio.isChecked()) {
+                        searchButton.setEnabled(true);
+                    }
+                }
+                else {
+                    searchButton.setEnabled(false);
+                }
+            }
+        };
+        tripButtonsGroup.setOnCheckedChangeListener(listener);
+
+        return v;
     }
+
+    public void onSearchClicked() {
+        searchButton.setEnabled(false);
+    }
+
+    private void returnSearchResult() {
+//        LoginRequest request = getLoginRequest();
+//
+//        LoginTask task = new LoginTask();
+//        task.setCallback(this);
+//        task.setHost(hostEdit.getText().toString());
+//        task.setPort(portEdit.getText().toString());
+//        task.execute(request);
+
+        // FIND MATCHING THINGS IN THE MOCK DATA
+        // SWITCH ACTIVITIES?? - SET THINGS IN THE MODEL TO INDICATE THAT THERE ARE SEARCH RESULTS
+    }
+
+    //-----------------------------------------------------------------//
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnSearchFlightsSelectedListener) {
-            mListener = (OnSearchFlightsSelectedListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnSearchFlightsSelectedListener");
-        }
+//        if (context instanceof OnSearchFlightsSelectedListener) {
+//            mListener = (OnSearchFlightsSelectedListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnSearchFlightsSelectedListener");
+//        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+//        mListener = null;
     }
+
 
     /**
      * This interface must be implemented by activities that contain this
